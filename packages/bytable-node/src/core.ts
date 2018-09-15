b5fc860bb39a48a87fd36b3739eafd1c8389354b
Dima Doronin <doronindm@gmail.com>
2018-09-15 14:50:40 -0400

    use cpp

--- packages/bytable-node/src/core.ts
+++ packages/bytable-node/src/core.ts
@@ -10,5 +10,4 @@ var Long = BSON.Long;
 // use them to serialize and deserialize the types.
 var bson = new BSON([BSON.Binary, BSON.Code, BSON.DBRef, BSON.Decimal128, BSON.Double, BSON.Int32, BSON.Long, BSON.Map, BSON.MaxKey, BSON.MinKey, BSON.ObjectId, BSON.BSONRegExp, BSON.Symbol, BSON.Timestamp]);
 
-console.info(typeof bson);
 export { bson };
