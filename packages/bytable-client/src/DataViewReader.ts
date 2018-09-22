ecf349aab6a045e478641877f527900f7adbe44e
Dima Doronin <doronindm@gmail.com>
2018-09-22 10:56:25 -0400

    use data view

--- packages/bytable-client/src/DataViewReader.ts
+++ packages/bytable-client/src/DataViewReader.ts
@@ -1,35 +1,23 @@
 import { Reader, byteMap } from 'bytable';
-import { BSON } from 'bson';
 import { Buffer } from 'buffer';
-
-const bson = new BSON();
+import { bson } from './core';
 
 export class DataViewReader<C> extends Reader<C, DataView> {
     readAs<T>(msg: DataView, type: string, offset: number, size: number): T {
         if(byteMap.has(type)) {
             switch(type){
-                case 'UInt8': 
-                    return msg.getUint8(offset) as any;
-                case 'UInt16BE':
-                    return msg.getUint16(offset) as any;
-                case 'UInt32BE':
-                    return msg.getUint32(offset) as any;
-
-                case 'Int8': 
-                    return msg.getInt8(offset) as any;
-                case 'Int16BE':
-                    return msg.getInt16(offset) as any;
-                case 'Int32BE':
-                    return msg.getInt32(offset) as any;
-
-                case 'FloatBE': 
-                    return msg.getFloat32(offset) as any;
-                case 'DoubleBE':
-                    return msg.getFloat64(offset) as any;
+                case 'UInt8':       return msg.getUint8(offset) as any;
+                case 'UInt16BE':    return msg.getUint16(offset) as any;
+                case 'UInt32BE':    return msg.getUint32(offset) as any;
+                case 'Int8':        return msg.getInt8(offset) as any;
+                case 'Int16BE':     return msg.getInt16(offset) as any;
+                case 'Int32BE':     return msg.getInt32(offset) as any;
+                case 'FloatBE':     return msg.getFloat32(offset) as any;
+                case 'DoubleBE':    return msg.getFloat64(offset) as any;
             }
         }
         const buf = msg.buffer.slice(offset, offset + size);
-        if(type === 'BSON') {
+        if (type === 'BSON') {
             return bson.deserialize(Buffer.from(buf));
         }
         return Buffer.from(buf).toString('UTF8') as any;
--- packages/bytable-client/src/DataViewWriter.ts
+++ packages/bytable-client/src/DataViewWriter.ts
@@ -1,26 +1,30 @@
 import { Writer } from 'bytable';
-import { BSON } from 'bson';
 import { Buffer } from 'buffer';
+import { bson } from './core';
 
-const bson = new BSON();
-
-// TODO: Should not rely on Buffer!!!
-export class DataViewWriter<T> extends Writer<T, ArrayBuffer> {
-    protected alloc(size: number): ArrayBuffer {
-        return Buffer.from(new ArrayBuffer(size)) as any;
+export class DataViewWriter<T> extends Writer<T, DataView> {
+    protected alloc(size: number): DataView {
+        return new DataView(new ArrayBuffer(size));
     }
 
     protected dynamicToBinary(type: string, value: any): any {
-        if(type === 'BSON') {
-            return bson.serialize(Buffer.from(value));
-        }
-        if(type === 'String') {
-            return Buffer.from(value as string, 'UTF8');
-        }
+        if(type === 'BSON')     return bson.serialize(value);
+        if(type === 'String')   return Buffer.from(value as string, 'UTF8');
         return null;
     }
-    protected writeAs(m: ArrayBuffer, type: string, value: any, shift: number): any {
-        if(type === 'binary') (m as any).fill(value as Buffer, shift);
-        else                  (m as any)[`write${type}`](value, shift);
+
+    protected writeAs(msg: DataView, type: string, value: any, offset: number): any {
+        switch(type){
+            case 'UInt8':       return msg.setUint8(offset, value) as any;
+            case 'UInt16BE':    return msg.setUint16(offset, value) as any;
+            case 'UInt32BE':    return msg.setUint32(offset, value) as any;
+            case 'Int8':        return msg.setInt8(offset, value) as any;
+            case 'Int16BE':     return msg.setInt16(offset, value) as any;
+            case 'Int32BE':     return msg.setInt32(offset, value) as any;
+            case 'FloatBE':     return msg.setFloat32(offset, value) as any;
+            case 'DoubleBE':    return msg.setFloat64(offset, value) as any;
+        }
+        if(type === 'binary')   return Buffer.from(msg.buffer as any).fill(value, offset).buffer;
+        else                    throw new Error(`Unknown type ${type}.`);
     }
 }
